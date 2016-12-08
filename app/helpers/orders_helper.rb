560650b41111403733775f9424c9b285ad95ce86
qizhengxin <qizhengxin721@gmail.com>
2016-12-08 13:26:15 +0800

    haha

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,6 +1,8 @@
 module OrdersHelper
   def render_order_paid_state(order)
+                             #外界往括号内的那东西传变量用的
     if order.is_paid?
+      #这的 order 就是 上面（order）里的
       "已付款"
     else
       "未付款"
